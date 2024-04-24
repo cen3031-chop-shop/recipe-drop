441d4b960facf52b0e2bda3265b02e4fb21c7090
vijjearalong <vijjearalong@ufl.edu>
2024-04-24 11:43:42 -0400

    create

---
+++ recipe-drop/src/app/create/page.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import BaseLayout from '@/components/baselayout'
+const Create = () => {
+    return (
+        <BaseLayout>
+        <div> Create Page</div>
+        </BaseLayout>
+    
+      )
+    }
+
+export default Create
